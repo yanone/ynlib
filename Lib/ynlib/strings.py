2f075055fed4bef6fe877fb5de1cfdf34488b525
Yanone <post@yanone.de>
2014-01-06 13:35:13 -0800

    Localized date and time string functions to German

--- Lib/ynlib/strings.py
+++ Lib/ynlib/strings.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
 def ReadableFileSize(num):
 	u"""\
 	Makes human readable file size of integer bytes.
@@ -65,12 +68,13 @@ def FormattedDate(timestamp, locale = 'en'):
 		return time.strftime(day + ". " + datelocale[locale][time.strftime("%b", time.localtime(timestamp))] + " %Y", time.localtime(timestamp))
 
 
-def NaturalWeedkayTimeAndDate(timestamp):
+def NaturalWeedkayTimeAndDate(timestamp, locale = 'en'):
 	u"""\
 	Return date and time as:
 	Wednesday, October 20th, 2010 at 14:12
 	"""
 	import time
+	from calendars import datelocale
 
 	# day without leading zero
 	day = time.strftime("%d", time.localtime(timestamp))
@@ -82,9 +86,12 @@ def NaturalWeedkayTimeAndDate(timestamp):
 	if int(day) in ordinals:
 		ordinal = ordinals[int(day)]
 	
-	return time.strftime("%A, %B " + day + ordinal + ", %Y at %H:%M", time.localtime(timestamp))
+	if locale == 'en':
+		return time.strftime("%A, %B " + day + ordinal + ", %Y at %H:%M", time.localtime(timestamp))
+	elif locale == 'de':
+		return time.strftime(datelocale[locale][time.strftime("%a", time.localtime(timestamp))] + ", " + day + ". " + datelocale[locale][time.strftime("%b", time.localtime(timestamp))] + " %Y um %H:%M Uhr", time.localtime(timestamp))
 
-def NaturalRelativeWeedkayTimeAndDate(timestamp):
+def NaturalRelativeWeedkayTimeAndDate(timestamp, locale = 'en'):
 	u"""\
 	Return date and time relative to current moment as:
 	- x seconds ago
@@ -93,6 +100,7 @@ def NaturalRelativeWeedkayTimeAndDate(timestamp):
 	- Wednesday, October 20th, 2010 at 14:12
 	"""
 	import time
+	answer = {}
 	
 	now = time.time()
 	timepassed = now - timestamp
@@ -100,27 +108,35 @@ def NaturalRelativeWeedkayTimeAndDate(timestamp):
 	if timepassed < 60: # less than 1 minute
 		seconds = int(timepassed)
 		if seconds == 1:
-			return "1 second ago"
+			answer['en'] = "1 second ago"
+			answer['de'] = "vor 1 Sekunde"
 		else:
-			return "%s seconds ago" % (seconds)
+			answer['en'] = "%s seconds ago" % (seconds)
+			answer['de'] = "vor %s Sekunden" % (seconds)
 	elif 60 < timepassed < (60 * 60): # 22 minutes ago
 		minutes = int(timepassed // 60)
 		if minutes == 1:
-			return "1 minute ago"
+			answer['en'] = "1 minute ago"
+			answer['de'] = "vor 1 Minute"
 		else:
-			return "%s minutes ago" % (minutes)
+			answer['en'] = "%s minutes ago" % (minutes)
+			answer['de'] = "vor %s Minuten" % (minutes)
 	elif (60 * 60) < timepassed < (60 * 60 * 24 * 1): # 22 hours ago
 		hours = int(timepassed // (60 * 60))
 		if hours == 1:
-			return "1 hour ago"
+			answer['en'] = "1 hour ago"
+			answer['de'] = "vor 1 Stunde"
 		else:
-			return "%s hours ago" % (hours)
-			
+			answer['en'] = "%s hours ago" % (hours)
+			answer['de'] = "vor %s Stunden" % (hours)
+
+	if answer.has_key(locale):
+		return answer[locale]
 	else:
-		return NaturalWeedkayTimeAndDate(timestamp)
+		return NaturalWeedkayTimeAndDate(timestamp, locale)
 
 
-def NaturalAmountOfTime(seconds):
+def NaturalAmountOfTime(seconds, locale = 'en'):
 	u"""\
 	Return years, months, days, hours, minutes and seconds of certain amount of seconds.
 	over 2 months
@@ -128,6 +144,7 @@ def NaturalAmountOfTime(seconds):
 	over 3 hours
 	"""
 
+	answer = {}
 
 	second = 1
 	minute = 60 * second
@@ -139,39 +156,52 @@ def NaturalAmountOfTime(seconds):
 	if seconds > year:
 		years = seconds // year
 		if years > 1:
-			return "over %s years" % (years)
+			answer['en'] = "over %s years" % (years)
+			answer['de'] = u"über %s Jahre" % (years)
 		else:
-			return "over %s year" % (years)
+			answer['en'] = "over 1 year"
+			answer['de'] = u"über 1 Jahr"
 	elif seconds > month:
 		months = seconds // month
 		if months > 1:
-			return "over %s months" % (months)
+			answer['en'] = "over %s months" % (months)
+			answer['de'] = u"über %s Monate" % (months)
 		else:
-			return "over %s month" % (months)
+			answer['en'] = "over 1 month"
+			answer['de'] = u"über 1 Monat"
 	elif seconds > day:
 		days = seconds // day
 		if days > 1:
-			return "over %s days" % (days)
+			answer['en'] = "over %s days" % (days)
+			answer['de'] = u"über %s Tage" % (days)
 		else:
-			return "over %s day" % (days)
+			answer['en'] = "over 1 day"
+			answer['de'] = u"über 1 Tag"
 	elif seconds > hour:
 		hours = seconds // hour
 		if hours > 1:
-			return "over %s hours" % (hours)
+			answer['en'] = "over %s hours" % (hours)
+			answer['de'] = u"über %s Stunden" % (hours)
 		else:
-			return "over %s hour" % (hours)
+			answer['en'] = "over 1 hour"
+			answer['de'] = u"über 1 Stunde"
 	elif seconds > minute:
 		minutes = seconds // minute
 		if minutes > 1:
-			return "over %s minutes" % (minutes)
+			answer['en'] = "over %s minutes" % (minutes)
+			answer['de'] = u"über %s Minuten" % (minutes)
 		else:
-			return "over %s minute" % (minutes)
+			answer['en'] = "over 1 minute" % (minutes)
+			answer['de'] = u"über 1 Minute" % (minutes)
 	else:
 		if seconds > 1:
-			return "%s seconds" % (seconds)
+			answer['en'] = "%s seconds" % (seconds)
+			answer['de'] = "%s Sekunden" % (seconds)
 		else:
-			return "%s second" % (seconds)
+			answer['en'] = "%s second" % (seconds)
+			answer['de'] = "%s Sekunde" % (seconds)
 
+	return answer[locale]
 
 def Garbage(length, uppercase = True, lowercase = True, numbers = True, punctuation = False):
 	u"""\
