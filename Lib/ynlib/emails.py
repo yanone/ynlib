d342056bbf7be7ce107190723231d9153629f1e3
Yanone <post@yanone.de>
2014-01-06 13:34:48 -0800

    Added basic Email class

--- Lib/ynlib/emails.py
+++ Lib/ynlib/emails.py
@@ -1,8 +1,4 @@
 def SendEmail(recipients, sender, subject, body, attachments = None):
-	u"""\
-	Send email.
-	recipients = ('post@example.com', 'another@example.com')
-	"""
 
 	# Import smtplib for the actual sending function
 	import smtplib
@@ -21,3 +17,37 @@ def SendEmail(recipients, sender, subject, body, attachments = None):
 	s = smtplib.SMTP('localhost')
 	s.sendmail(sender, recipients, msg.as_string())
 	s.quit()
+
+
+
+
+class Email(object):
+	u"""\
+	Send email.
+	recipients = ('post@example.com', 'another@example.com')
+	"""
+
+	def __init__(self):
+		self.recipients = []
+		self.sender = ''
+		self.sendername = ''
+		self.subject = ''
+		self.body = ''
+		self.attachments = []
+
+	def send(self):
+		
+		# Import smtplib for the actual sending function
+		import smtplib
+
+		# Import the email modules we'll need
+		from email.mime.text import MIMEText
+		
+		msg = MIMEText(self.body)
+		msg['Subject'] = self.subject
+		msg['From'] = self.sender
+		msg['To'] = ','.join(recipients)
+
+		s = smtplib.SMTP('localhost')
+		s.sendmail(sender, recipients, msg.as_string())
+		s.quit()
