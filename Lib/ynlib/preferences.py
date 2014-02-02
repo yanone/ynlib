3f74aa8643ffc5e61567b508a949eaa59636d061
Yanone <post@yanone.de>
2014-02-02 10:34:08 -0800

    Pimped

--- Lib/ynlib/preferences.py
+++ Lib/ynlib/preferences.py
@@ -1,22 +1,41 @@
 import plistlib, os
 
 class Preferences(object):
-	def __init__(self, appKey):
+	def __init__(self, appKey = None, path = None):
 		self.appKey = appKey
 		self.preferences = {}
 
 		# plist
-		self.plistfile = os.path.join(os.path.expanduser("~/Library/Preferences/"), self.appKey + '.plist')
+		if path:
+			self.plistfile = path
+		else:
+			self.plistfile = os.path.join(os.path.expanduser("~/Library/Preferences/"), self.appKey + '.plist')
 		
 		self.loadPreferences()
+		
+		self.modified = False
 		
 	def get(self, key):
 		if self.preferences.has_key(key):
 			return self.preferences[key]
 		
-	def set(self, key, value):
+	def put(self, key, value):
+		if value != self.get(key):
+			self.modified = True
+
 		self.preferences[key] = value
-		self.savePreferences()
+
+		if self.modified == True:
+			self.savePreferences()
+
+	def max(self, key, value):
+		
+		if value > self.get(key):
+			self.put(key, value)
+
+	def min(self, key, value):
+		if value < self.get(key):
+			self.put(key, value)
 	
 	def delete(self, key):
 		if self.preferences.has_key(key):
