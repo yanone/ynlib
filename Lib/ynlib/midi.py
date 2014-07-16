093610c1c339b10a8c5891b3bcd6be29ecbb5b59
Yanone <post@yanone.de>
2014-07-16 15:18:16 +0200

    Listen in separate process.

--- Lib/ynlib/midi.py
+++ Lib/ynlib/midi.py
@@ -1,5 +1,22 @@
 import rtmidi, time
 
+import threading
+class midiListener(threading.Thread): 
+	def __init__(self, midi): 
+		threading.Thread.__init__(self) 
+		self.midi = midi
+ 
+	def run(self): 
+		self.midi.midiin.openPort(0)
+		while True:
+			time.sleep(.1)
+		self.midi.midiin.closePort()
+
+	def stop(self):
+		self._Thread__stop()
+
+
+
 class MidiWithCallBack:
 	def __init__(self):
 		
@@ -11,12 +28,22 @@ class MidiWithCallBack:
 		# Start listening
 		self.midiin.setCallback(self.callFunction)
 
+		self.listener = midiListener(self)
+
 	def startListening(self):
 		'''Start listenting'''
-		self.midiin.openPort(0)
-		while True:
-			time.sleep(.1)
-		self.midiin.closePort()
+
+
+		self.listener.start()
+
+
+#		self.midiin.openPort(0)
+#		while True:
+#			time.sleep(.1)
+#		self.midiin.closePort()
+
+	def stop(self):
+		self.listener.stop()
 
 	def registerFunction(self, function, channel = None):
 		'''Map function to MIDI channels'''
@@ -29,7 +56,15 @@ class MidiWithCallBack:
 	def callFunction(self, midi):
 		'''Call registered functions'''
 		
-		channel = midi.getControllerNumber()
+		if midi.isNoteOn():
+			channel = midi.getMidiNoteName(midi.getNoteNumber())
+			value = midi.getVelocity()
+		elif midi.isNoteOff():
+			channel = midi.getMidiNoteName(midi.getNoteNumber())
+			value = 0
+		elif midi.isController():
+			channel = midi.getControllerNumber()
+			value = midi.getControllerValue()
 		
 		# Default mapping
 		if not self.functions.has_key(channel):
@@ -40,4 +75,4 @@ class MidiWithCallBack:
 			f = self.functions[channel]
 			#print f
 			#eval(f.__name__ + '(midi)')
-			f(midi)
+			f(channel, value)
